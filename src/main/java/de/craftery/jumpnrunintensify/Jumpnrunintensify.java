a895a8ce5277eb1dcc37200c6b81a3ffe0d3dbb3
Crafter-Y <mrcrafter.yt1@gmail.com>
2025-06-22 20:41:43 +0200

    Ready. Set. Go!

---
+++ src/main/java/de/craftery/jumpnrunintensify/Jumpnrunintensify.java
@@ -0,0 +1,17 @@
+package de.craftery.jumpnrunintensify;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Jumpnrunintensify extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
